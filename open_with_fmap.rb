059114d2b85ffd9d4ccde9544cbdbbe215c4c399
jose.luisdiaz <jose.luisdiaz@yello.co>
2020-02-11 21:30:45 -0500

    test pattern matching results

--- open_with_fmap.rb
+++ open_with_fmap.rb
@@ -8,7 +8,7 @@ class OpenWithFmap
   include Dry::Monads[:try, :result]
 
   def call(**input)
-    StateMachines::FetchDevice.(input)
+    result = StateMachines::FetchDevice.(input)
       .fmap(DecisionMaker::GetNextStates)
       .bind(DecisionMaker::UpdateStates)
       .bind(DecisionMaker::IncreaseCounter)
@@ -19,6 +19,17 @@ class OpenWithFmap
           .to_result
           .fmap { acc }
       end
+    process_result(result)
+  end
+
+  def process_result(result)
+    case result
+    in Failure(:device_not_found)
+    in Failure(IotClient::DeviceError)
+    in Some(Float | String)
+    in Success(_)
+      Success(:ok)
+    end
   end
 
 end
